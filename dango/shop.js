18d9c663b958357db38382390ad445d72cc4753d
sutegosaurusu <kdaikichi1016@gmail.com>
2026-08-03 11:55:11 +0900

    Create shop.js

---
+++ dango/shop.js
@@ -0,0 +1,85 @@
+let currentMember = null;
+
+
+auth.onAuthStateChanged(async user => {
+
+    if(!user){
+        alert("先にログインしてください");
+        location.href = "../index.html";
+        return;
+    }
+
+    currentUser = user;
+
+
+    const memberSnapshot = await database
+        .ref("members/" + user.uid)
+        .once("value");
+
+
+    currentMember = memberSnapshot.val();
+
+
+    if(!currentMember){
+        showMessage("会員情報が見つかりません");
+        return;
+    }
+
+
+    updatePointDisplay();
+
+    watchMember();
+    watchInventory();
+    watchListings();
+
+});
+
+
+
+function watchMember(){
+
+    database
+        .ref("members/" + currentUser.uid)
+        .on("value", snapshot => {
+
+            currentMember = snapshot.val() || {};
+
+            updatePointDisplay();
+
+        });
+
+}
+
+
+
+function updatePointDisplay(){
+
+    const point =
+        Number(currentMember?.point || 0);
+
+
+    document.getElementById("pointDisplay")
+    .textContent =
+        point.toLocaleString() + " Pt";
+
+}
+
+
+
+function watchInventory(){
+
+    database
+        .ref("inventories/" + currentUser.uid)
+        .on("value", snapshot => {
+
+            inventoryData =
+                snapshot.val() || {};
+
+
+            renderSellItems();
+
+            renderMarket();
+
+        });
+
+}
