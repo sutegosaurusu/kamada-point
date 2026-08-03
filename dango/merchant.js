ddcf19aa2f1fa07953781139c2099b038d65e369
sutegosaurusu <kdaikichi1016@gmail.com>
2026-08-03 11:31:33 +0900

    Update merchant.js

--- dango/merchant.js
+++ dango/merchant.js
@@ -1,34 +1,11 @@
-// ======================
-// 商人販売商品一覧
-// ======================
-
-// 通常商人
 const merchantItems = [
-
-    "merchant_apple",
-    "merchant_cucumber",
-    "merchant_watermelon",
     "merchant_leaf",
-    "merchant_jelly",
-    "merchant_chinese_cabbage",
-    "merchant_azuki_bar",
-    "merchant_honey",
-
-    "merchant_water",
-    "merchant_slime",
-    "merchant_os1"
-
+    "merchant_water"
 ];
-
-
-// ======================
-// 商人商品取得
-// ======================
 
 function getMerchantItems(){
 
     return merchantItems
-        .map(id => items.find(item => item.id === id))
-        .filter(item => item);
-
+    .map(id => items.find(item => item.id === id))
+    .filter(item => item);
 }
