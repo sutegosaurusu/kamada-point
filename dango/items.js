3f917edfc49d2d6502c34e81f6625c755ddeaf25
sutegosaurusu <kdaikichi1016@gmail.com>
2026-08-03 10:24:14 +0900

    Update items.js

--- dango/items.js
+++ dango/items.js
@@ -1,1 +1,148 @@
+const merchantItems = [
+
+    // ======================
+    // 食べ物
+    // ======================
+
+    {
+        id:"merchant_apple",
+        name:"リンゴ",
+        category:"food",
+        price:120
+    },
+    {
+        id:"merchant_cucumber",
+        name:"キュウリ",
+        category:"food",
+        price:80
+    },
+    {
+        id:"merchant_watermelon",
+        name:"スイカ",
+        category:"food",
+        price:450
+    },
+    {
+        id:"merchant_leaf",
+        name:"落ち葉",
+        category:"food",
+        price:15
+    },
+    {
+        id:"merchant_jelly",
+        name:"ゼリー",
+        category:"food",
+        price:100
+    },
+    {
+        id:"merchant_chinese_cabbage",
+        name:"白菜",
+        category:"food",
+        price:180
+    },
+    {
+        id:"merchant_azuki_bar",
+        name:"小豆バー",
+        category:"food",
+        price:160
+    },
+    {
+        id:"merchant_honey",
+        name:"蜜",
+        category:"food",
+        price:300
+    },
+
+    // ======================
+    // 水
+    // ======================
+
+    {
+        id:"merchant_water",
+        name:"水",
+        category:"water",
+        price:40
+    },
+    {
+        id:"merchant_slime",
+        name:"粘液",
+        category:"water",
+        price:90
+    },
+    {
+        id:"merchant_os1",
+        name:"OS-1",
+        category:"water",
+        price:250
+    },
+
+
+// ======================
+// 装備
+// ======================
+
+const equipmentItems = [
+
+    {
+        id: "",
+        name: "",
+        category: "equipment",
+        price: 0
+    },
+
+    {
+        id: "",
+        name: "",
+        category: "equipment",
+        price: 0
+    }
+
+];
+
+
+// ======================
+// 素材
+// ======================
+
+const materialItems = [
+
+    {
+        id: "",
+        name: "",
+        category: "material",
+        price: 0
+    },
+
+    {
+        id: "",
+        name: "",
+        category: "material",
+        price: 0
+    }
+
+];
+
+
+// ======================
+// 道具
+// ======================
+
+const toolItems = [
+
+    {
+        id: "",
+        name: "",
+        category: "tool",
+        price: 0
+    },
+
+    {
+        id: "",
+        name: "",
+        category: "tool",
+        price: 0
+    }
+
+];
+
 
