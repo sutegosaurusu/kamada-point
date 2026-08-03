c813a971b7788f00c739284ebc535b1cfff99d99
sutegosaurusu <kdaikichi1016@gmail.com>
2026-08-03 11:52:35 +0900

    Update firebase.js

--- dango/firebase.js
+++ dango/firebase.js
@@ -1,13 +1,21 @@
 // Firebase設定
 
 const firebaseConfig = {
-    apiKey: "ここに自分の値",
-    authDomain: "ここに自分の値",
-    databaseURL: "ここに自分の値",
-    projectId: "ここに自分の値",
-    storageBucket: "ここに自分の値",
-    messagingSenderId: "ここに自分の値",
-    appId: "ここに自分の値"
+
+apiKey:"AIzaSyBgILQJKJOiQ6gIeZPKhbVjdybXS_vsD3Q",
+
+authDomain:"kamadapoint.firebaseapp.com",
+
+databaseURL:"https://kamadapoint-default-rtdb.asia-southeast1.firebasedatabase.app",
+
+projectId:"kamadapoint",
+
+storageBucket:"kamadapoint.firebasestorage.app",
+
+messagingSenderId:"601769416524",
+
+appId:"1:601769416524:web:0be49ce5a0baf87f75e1e9"
+
 };
 
 
