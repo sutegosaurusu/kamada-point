3ee1d76efc5708c0aaf650332263a2613ff72ed2
sutegosaurusu <kdaikichi1016@gmail.com>
2026-08-05 20:58:25 +0900

    Update locations.js

--- dango/locations.js
+++ dango/locations.js
@@ -40,7 +40,7 @@ const locations = [
         { itemId:"toothpick", weight:1 },
         { itemId:"pull_tab", weight:0.7 },
         { itemId:"bottle_cap", weight:0.5 },
-        { itemId:"bottle_cap", weight:0.5 },
+        { itemId:"Anteggs", weight:0.5 },
         { itemId:"bottle_cap", weight:0.5 },
         { itemId:"bottle_cap", weight:0.5 },
         { itemId:"stag_beetle_jaw", weight:0.3 }
