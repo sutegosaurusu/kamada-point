cfecd6d6361eeed2577eddb2d385d49413b6805b
sutegosaurusu <kdaikichi1016@gmail.com>
2026-08-06 07:11:37 +0900

    Update merchant.js

--- dango/merchant.js
+++ dango/merchant.js
@@ -3,7 +3,11 @@ window.merchantItems = items.map(item => item.id);
 function getMerchantItems(){
 
     return window.merchantItems
-    .map(id => items.find(item => item.id === id))
-    .filter(item => item);
+        .map(id => items.find(item => item.id === id))
+        .filter(item => item)
+        .map(item => ({
+            ...item,
+            sellPrice: Math.floor(item.price * 0.3)
+        }));
 
 }
